e5314c53187525316527772b06f7ab1e6f8f6280
Vincent <wolfvincent1501@gmail.com>
2024-05-07 18:09:01 +0200

    added JVM argument utilities; added the arguments: -timestamp_prefix and -verbose

---
+++ src/main/java/me/vault/vaultgame/utility/Constants.java
@@ -0,0 +1,27 @@
+package me.vault.vaultgame.utility;
+
+
+/**
+ * This interface provides various constants that are used within many different classes of this program,
+ * because literals should be avoided within the code directly.
+ *
+ * @author Vincent Wolf
+ */
+public interface Constants {
+    /**
+     * A constant to represent an empty string.
+     */
+    String EMPTY_STRING = "";
+
+
+    /**
+     * A constant to represent a whitespace character.
+     */
+    char WHITESPACE = ' ';
+
+
+    /**
+     * A constant to represent a dash character.
+     */
+    char DASH = '-';
+}
