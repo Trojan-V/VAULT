bc80801e68f72980f077b10f4431405640d4ec2a
Vincent <wolfvincent1501@gmail.com>
2024-05-07 23:50:12 +0200

    overhaul of the city model etc.

--- src/main/java/me/vault/vaultgame/utility/Constants.java
+++ src/main/java/me/vault/vaultgame/utility/Constants.java
@@ -2,26 +2,22 @@ package me.vault.vaultgame.utility;
 
 
 /**
- * This interface provides various constants that are used within many different classes of this program,
- * because literals should be avoided within the code directly.
+ * This interface provides various constants that are used within many different classes of this program, because
+ * literals should be avoided within the code directly.
  *
  * @author Vincent Wolf
+ * @since 07.05.2024
  */
-public interface Constants {
-    /**
-     * A constant to represent an empty string.
-     */
-    String EMPTY_STRING = "";
+public interface Constants
+{
+	/**
+	 * A constant to represent a whitespace character.
+	 */
+	char WHITESPACE = ' ';
 
 
-    /**
-     * A constant to represent a whitespace character.
-     */
-    char WHITESPACE = ' ';
-
-
-    /**
-     * A constant to represent a dash character.
-     */
-    char DASH = '-';
+	/**
+	 * A constant to represent a dash character.
+	 */
+	char DASH = '-';
 }
