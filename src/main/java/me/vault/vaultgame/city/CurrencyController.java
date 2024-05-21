03d35e3b18241c90dc55c7458fc6d6a1e5cd95f5
Lasse27 <llhillen1403@gmail.com>
2024-05-21 22:34:48 +0200

    JAVADOC für CurrencyController.java geschrieben

--- src/main/java/me/vault/vaultgame/city/CurrencyController.java
+++ src/main/java/me/vault/vaultgame/city/CurrencyController.java
@@ -1,7 +1,19 @@
 package me.vault.vaultgame.city;
 
 
-public class CurrencyController
+/**
+ * The {@code CurrencyController} class primarily provides methods and isn't meant to be implemented. It's used to
+ * handle the {@link Currency} class.
+ *
+ * @author Lasse-Leander Hillen
+ * @see Currency
+ * @since 21.05.2024
+ */
+public final class CurrencyController
 {
-
+	/**
+	 * Private constructor since the class isn't supposed to be instantiated.
+	 */
+	private CurrencyController ()
+	{}
 }
