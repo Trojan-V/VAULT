53fbbcfb88376c86d1f978eca86180e041146025
T.H <167987663+SurpriseDecentralizedBackup@users.noreply.github.com>
2024-06-08 22:32:09 +0200

    Add String prologue to Interface StringConstants; Add prologue text to initialize method of PrologueController

--- src/main/java/me/vault/game/PrologueController.java
+++ src/main/java/me/vault/game/PrologueController.java
@@ -4,8 +4,10 @@ package me.vault.game;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
 import javafx.scene.image.ImageView;
 import me.vault.game.city.CityView;
+import me.vault.game.utility.constant.StringConstants;
 import me.vault.game.utility.loading.ResourceLoader;
 
 import java.net.URL;
@@ -20,6 +22,9 @@ public class PrologueController implements Initializable
 	@FXML
 	private ImageView prologueImageView;
 
+	@FXML
+	private Label storyText;
+
 
 	@FXML
 	void continueButtonClick (final ActionEvent ignored)
@@ -32,5 +37,6 @@ public class PrologueController implements Initializable
 	public void initialize (final URL url, final ResourceBundle resourceBundle)
 	{
 		this.prologueImageView.setImage(ResourceLoader.loadImage(ASSETS_PATH + CITY_BACKGROUND_FILENAME));
+		this.storyText.setText(StringConstants.prologue);
 	}
 }
--- src/main/java/me/vault/game/utility/constant/StringConstants.java
+++ src/main/java/me/vault/game/utility/constant/StringConstants.java
@@ -3,4 +3,13 @@ package me.vault.game.utility.constant;
 
 public interface StringConstants
 {
+	public static String prologue =
+		"Wir schreiben das Jahr 2341 und die Menschheit hat sich in den letzten Jahrhunderten in der Galaxie " +
+		"ausgebreitet. W\u00E4hrend dieser Zeit wurden die viele neue Systeme erforscht und wissenschaftliche Entdeckungen gemacht. Eine der relevantesten Entdeckungen war die Entdeckung" +
+		" der technologisch fortschrittlichen Zivilsation &quot;Hitani&quot;, die unter anderem ein weitreichendes galaktisches Teleportationssystem entwickelt haben. Mit der Erschlie\u00DFung " +
+		"dieser Technologie konnten die Menschen weitere Systeme entdecken und erkunden. Vor kurzem wurde ein neues Sonnensystem, Proxima Tau, entdeckt und eine Mission unter der Leitung von " +
+		"James Vault wurde entsand um diese neuen Welten zu erkunden. Nach der Ankunft in Proxima Tau ankommen stellt James Vault fest, dass er sich auf einer hitanischen Raumstation befindet. " +
+		"Wie bei vielen in der Vergangenheit erkundeten Systemen war auch Proxima Tau schon bereits bev\u00F6lkert. Doch schon bald entdeckte das Team eine schwerwiegendes Problem. " +
+		"Das Transportsystem auf das das Team " +
+		"f\u00FCr ihre R\u00FCckreise angewiesen waren, hatte eine Fehlst\u00F6rung und muss repariert werden.";
 }
