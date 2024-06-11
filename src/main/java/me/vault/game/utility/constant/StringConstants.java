4c7f76a60032096ff4ca2be12d8a8448bbd6ade5
Alexander <goethel.alexander@gmail.com>
2024-06-11 12:24:42 +0200

    Texts for the other half of the tutorial added and added comments

--- src/main/java/me/vault/game/utility/constant/StringConstants.java
+++ src/main/java/me/vault/game/utility/constant/StringConstants.java
@@ -263,12 +263,32 @@ public interface StringConstants
 
 
 	/**
-	 *
+	 * The following Strings contain the texts for the tutorial,
 	 */
-	public static String tutorialIntroduction = "Text for the introduction";
+	public static String tutorialIntroduction = "Hello dear player, first thank you for playing our little game. In " +
+	                                            "the following tabs you can read the tutorials of the integral parts" +
+	                                            " " +
+	                                            "of the game. They describe what certain things are for and how the " +
+	                                            "mechanics of this game work in a capacity that helps you understand" +
+	                                            " " +
+	                                            "how one can approach the game.";
 
-	public static String tutorialCity = "Text for the city";
 
+	public static String tutorialCity = "The City is the main hub. At the top of the screen the current amount off " +
+	                                    "resources one has in its possession are displayed. Below that there are the" +
+	                                    " " +
+	                                    "buildings of the space station. There are the barracks for the own faction," +
+	                                    " " +
+	                                    "the docks and the space-bar for the other three factions, where one can " +
+	                                    "recruit troops of the different faction, unlocked through missions or are " +
+	                                    "unlocked from the start. The command center is where one selects the " +
+	                                    "mission" +
+	                                    " to play. The market can be used to exchange  resources with each other if " +
+	                                    "here are not enough to do something and one  don`t want to play a mission. " +
+	                                    "Lastly the training facility, workshop and the laboratory are there for " +
+	                                    "upgrades. In the training facility one can upgrade  unlocked troops. The " +
+	                                    "workshop improves the collected artifacts that one posses and the " +
+	                                    "laboratory can upgrade the ability of you, the player.";
 
 	public static String tutorialArtifacts = "Artifacts are mysterious pieces of equipment that were left behind by " +
 	                                         "the Hitani and survived the tests of time. These strange pieces of " +
@@ -276,14 +296,11 @@ public interface StringConstants
 	                                         "selected by the player per mission and buff damage, defence or health " +
 	                                         "for the troops that go one that mission.";
 
-
 	public static String tutorialFactsions = "In the system there are three faction besides to player-faction. The " +
 	                                         "troops of the factions can be used through different means and at " +
 	                                         "different points in the game. At the same time these factions also " +
-	                                         "pose" +
-	                                         " as the enemy combatants in the game, so it dose pay off to learn more" +
-	                                         " " +
-	                                         "about these once your in the game proper.";
+	                                         "pose as the enemy combatants in the game, so it dose pay off to learn " +
+	                                         "more about these once your in the game proper.";
 
 
 	public static String tutorialMissions = "The missions in this game are divided in two categories. The first kind" +
@@ -298,7 +315,20 @@ public interface StringConstants
 	                                        "to fight enemy's.";
 
 
-	public static String tutorialFights = "When you encounter ";
+	public static String tutorialFights = "When you encounter enemy`s on the mission-map you enter a fight. Fight in" +
+	                                      " " +
+	                                      "this game is turn-based and works on twelve times twelve grid with various" +
+	                                      " " +
+	                                      "environment based obstacles. After a troop attacks a hostile entity a " +
+	                                      "twenty sided dice is thrown to determine if the troop hits or if the " +
+	                                      "defendant hostile can dodge the attack. If an attack lands the damage " +
+	                                      "that" +
+	                                      " is done is modified by the armour or resistance of the defending troop. " +
+	                                      "The movement range determines how much tiles a unit can once and damage " +
+	                                      "and grenade range determines how great the distance there can be between " +
+	                                      "the attacker and the defender. If a troop is down to zero health it is " +
+	                                      "out" +
+	                                      " of combat.";
 
 	public static String buttonRoundImageName = "button_round.png";
 
