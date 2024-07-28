d2fb1e5d098dc102fea5d5986cdcbeaad31967ec
Alexander <goethel.alexander@gmail.com>
2024-07-28 19:23:18 +0200

    changed sprites for the energy ability`s

--- src/main/java/me/vault/game/utility/constant/EnergyConstants.java
+++ src/main/java/me/vault/game/utility/constant/EnergyConstants.java
@@ -34,10 +34,10 @@ public interface EnergyConstants
 		String IMPROVED_NAME = "Improved Dodge Ability";
 
 
-		String BASE_SPRITE_PATH = ASSETS_PATH + "artifact/defense_artifact_icon.png";
+		String BASE_SPRITE_PATH = ASSETS_PATH + "energy/dodge_energyability.png";
 
 
-		String IMPROVED_SPRITE_PATH = ASSETS_PATH + "artifact/defense_artifact_icon.png";
+		String IMPROVED_SPRITE_PATH = ASSETS_PATH + "energy/dodge_energyability.png";
 
 
 		CurrencyTransaction BASE_TO_IMPROVED_UPGRADE_COSTS = new CurrencyTransaction(-1000, -1000, -1000, -1000, -1000);
@@ -56,10 +56,10 @@ public interface EnergyConstants
 		String IMPROVED_NAME = "Improved Initiative Ability";
 
 
-		String BASE_SPRITE_PATH = ASSETS_PATH + "artifact/damage_artifact_icon.png";
+		String BASE_SPRITE_PATH = ASSETS_PATH + "energy/initiative_energyabiltiy.png";
 
 
-		String IMPROVED_SPRITE_PATH = ASSETS_PATH + "artifact/damage_artifact_icon.png";
+		String IMPROVED_SPRITE_PATH = ASSETS_PATH + "energy/initiative_energyabiltiy.png";
 
 
 		CurrencyTransaction BASE_TO_IMPROVED_UPGRADE_COSTS = new CurrencyTransaction(-1000, -1000, -1000, -1000, -1000);
@@ -72,16 +72,16 @@ public interface EnergyConstants
 	interface Melee
 	{
 
-		String BASE_NAME = "Melee Artifact";
+		String BASE_NAME = "Melee Ability";
 
 
-		String IMPROVED_NAME = "Melee Health Artifact";
+		String IMPROVED_NAME = "Improved Melee Ability";
 
 
-		String BASE_SPRITE_PATH = ASSETS_PATH + "artifact/health_artifact_icon.png";
+		String BASE_SPRITE_PATH = ASSETS_PATH + "energy/melee_energyability.png";
 
 
-		String IMPROVED_SPRITE_PATH = ASSETS_PATH + "artifact/health_artifact_icon.png";
+		String IMPROVED_SPRITE_PATH = ASSETS_PATH + "energy/melee_energyability.png";
 
 
 		CurrencyTransaction BASE_TO_IMPROVED_UPGRADE_COSTS = new CurrencyTransaction(-1000, -1000, -1000, -1000, -1000);
