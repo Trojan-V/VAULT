7033ab0d9b971f48f91ce0258ced5112a520e7b9
T.H <167987663+SurpriseDecentralizedBackup@users.noreply.github.com>
2024-07-29 18:25:38 +0200

    added JavaDoc to ResourceLoader; modularization of ResourceLoader.createTileArrayFromFile

--- src/main/java/me/vault/game/utility/loading/ResourceLoader.java
+++ src/main/java/me/vault/game/utility/loading/ResourceLoader.java
@@ -22,12 +22,12 @@ import static me.vault.game.utility.logging.ILogger.Level.WARNING;
 
 
 /**
- * This class is a utility class that provides methods to load images and scenes.
+ * This class is a utility class that provides methods to load Resources such as images and scenes.
  * <br>
  * After the loading process is complete, an instance of a Java object is returned to the method invoker.
  * This instance can be used to give JavaFX the proper data structure that it requires to load the image or scene.
  *
- * @author Vincent Wolf
+ * @author Vincent Wolf, Timothy Hoegen-Jupp
  * @version 1.0.0
  * @see MetaDataImage
  * @see Scene
@@ -69,6 +69,9 @@ public final class ResourceLoader
 	 *
 	 * @param resourcePath The path from which the resource (the image) is loaded.
 	 * @return An instance of {@link MetaDataImage}.
+	 *
+	 * @precondition
+	 * @postcondition the specified Image is returned; null if the resourcePath is not valid
 	 */
 	public static MetaDataImage loadImage (final String resourcePath)
 	{
@@ -97,6 +100,9 @@ public final class ResourceLoader
 	 * @param clazz            The class that corresponds to the view (fxml).
 	 * @param fxmlResourcePath The path from which the resource (the scene) is loaded.
 	 * @return An instance of {@link Scene}.
+	 *
+	 * @precondition The specified resource Path has to point to a valid FXML-File (not null)
+	 * @postcondition The specified Scene is returned
 	 */
 	public static <T> Scene loadScene (final Class<T> clazz, final String fxmlResourcePath)
 	{
@@ -120,12 +126,15 @@ public final class ResourceLoader
 	/**
 	 * Returns a new instance of {@link File} for the provided path.
 	 *
-	 * @param filePath The path of the file.
+	 * @param directoryPath The path of the file.
 	 * @return An instance {@link File}.
+	 *
+	 * @precondition The directoryPath has to point to a valid directory (not null)
+	 * @postcondition The directory is returned as a {@link File} Object
 	 */
-	public static File getDirectory (final String filePath)
+	public static File getDirectory (final String directoryPath)
 	{
-		return new File(filePath);
+		return new File(directoryPath);
 	}
 
 
@@ -134,6 +143,9 @@ public final class ResourceLoader
 	 *
 	 * @param directoryPath The path to the directory.
 	 * @return The {@link List} of files.
+	 *
+	 * @precondition the directoryPath has to point to a valid directory (not null)
+	 * @postcondition all files in the directory are returned
 	 */
 	public static List<File> collectFiles (final String directoryPath)
 	{
@@ -146,11 +158,24 @@ public final class ResourceLoader
 		return new ArrayList<>(List.of(files));
 	}
 
+
+	/**
+	 * Collects all file in the supplied directory that have the specified file-name ending and returns them as a
+	 * {@link List}.
+	 *
+	 * @param directoryPath The path to the directory
+	 * @param fileEnding The ending of the files that should be returned
+	 * @return A {@link List} of all files that have the specified ending
+	 *
+	 * @precondition The directoryPath has to point to a valid directory (not null) and the fileEnding cannot be null
+	 * @postcondition All files with the specified ending are returned
+	 */
 	public static List<File> collectFilesWithSpecifiedEnding (final String directoryPath, String fileEnding)
 	{
 		final File[] allFiles = getDirectory(directoryPath).listFiles();
 		List<File> validFiles = new ArrayList<>();
 
+		//checks the file ending of all files in the directory.
 		for (File file: allFiles)
 		{
 			if (file.getName().endsWith(fileEnding))
@@ -161,6 +186,19 @@ public final class ResourceLoader
 		return validFiles;
 	}
 
+
+	/**
+	 * Collects all files in the supplied directory that contain the specified pattern in the file name and returns
+	 * them as a {@link List}.
+	 *
+	 * @param directoryPath The path to the directory
+	 * @param pattern The pattern that should be contained within the filename
+	 * @return A {@link List} containing all files with the specified pattern in their filename
+	 *
+	 * @precondition The directoryPath has to point to a valid directory (not null) and the specified pattern cannot
+	 * be null
+	 * @postcondition All files with the specified pattern in the filename are returned
+	 */
 	public static List<File> collectFilesContaining (final String directoryPath, String pattern)
 	{
 		final File[] allFiles = getDirectory(directoryPath).listFiles();
@@ -176,6 +214,17 @@ public final class ResourceLoader
 		return validFiles;
 	}
 
+
+	/**
+	 * Retuns the specified {@link File}.
+	 *
+	 * @param directoryPath The path to the directory
+	 * @param fileName The name of the desired File
+	 * @return The File with the specified fileName from the specified directory
+	 *
+	 * @precondition The directoryPath has to point to a valid directory (not null).
+	 * @postcondition The File with the specified fileName; null if there is no file with the corresponding fileName
+	 */
 	public static File getFile (final String directoryPath, String fileName)
 	{
 		final File[] files = getDirectory(directoryPath).listFiles();
@@ -191,49 +240,136 @@ public final class ResourceLoader
 	}
 
 
+	/**
+	 * Creates a Tile-Array (Gameboard) from the specified File.
+	 * <br>
+	 * The file has to follow certain rules in order for this method to be able to properly read it. The number of
+	 * lines in the file has to match {@link me.vault.game.utility.constant.GameBoardConstants#GAME_BOARD_ROW_COUNT}
+	 * and the number of characters in each line have to match
+	 * {@link me.vault.game.utility.constant.GameBoardConstants#GAME_BOARD_COLUMN_COUNT}. Furthermore, the file has
+	 * to contain valid Tile representations such as
+	 * {@link me.vault.game.utility.constant.GameBoardConstants#OBSTACLE_TILE}.
+	 *
+	 * @param filePath The path to the file from which the Tile-Array should be constructed
+	 * @return TileArray that represents the content of the specified file
+	 *
+	 * @see me.vault.game.utility.constant.GameBoardConstants
+	 * @see BufferedReader
+	 * @see Tile
+	 *
+	 * @precondition The filePath has to point to a vaild file (not null) that is the right size and contains the
+	 * allowed characters and does not exceed the UTF-8 character range
+	 * @postcondition The Tile-Array representation of the file content is returned with the character-representations
+	 * having been exchanged for the Tile-Objects
+	 */
 	public static Tile[][] createTileArrayFromFile (final String filePath)
 	{
 		final Tile[][] gameBoard = new Tile[GAME_BOARD_ROW_COUNT][GAME_BOARD_COLUMN_COUNT];
+		final BufferedReader reader = createBufferedReaderFromFile(filePath);
 
-		try (
-			final BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(filePath),
-				StandardCharsets.UTF_8)))
+		//Throws an error if the number of lines in the file does not match the number of rows of the Gameboard
+		if (getNumberOfLinesInFile(filePath) != GAME_BOARD_ROW_COUNT)
 		{
-			for (int row = 0; row < GAME_BOARD_ROW_COUNT; row++)
-			{
-				final String line = reader.readLine();
-				if (line.length() != GAME_BOARD_ROW_COUNT)
-				{
-					throw new RuntimeException(); // TODO: EIGENE EXCEPTION
-				}
+			throw new RuntimeException(); //TODO: Eigene Exception
+		}
 
-				final char[] charArray = line.toCharArray();
-				for (int column = 0; column < GAME_BOARD_COLUMN_COUNT; column++)
+
+		for (int row = 0; row < GAME_BOARD_ROW_COUNT; row++)
+		{
+			final String line;
+
+			try
+			{
+				line = reader.readLine();
+			}
+			catch (IOException e)
+			{
+				throw new RuntimeException(e);
+			}
+
+			//Throws an exception if the number of characters in a line does not match the number of columns of the
+			// Gameboard
+			if (line.length() != GAME_BOARD_ROW_COUNT)
+			{
+				throw new RuntimeException(); // TODO: EIGENE EXCEPTION
+			}
+
+			final char[] charArray = line.toCharArray();
+
+			//goes through each column of the gameboard
+			for (int column = 0; column < GAME_BOARD_COLUMN_COUNT; column++)
+			{
+				// TODO: bundle these classes into an enum or sth
+				//determines the tile that is added to the gameboard from the file with the help of characters
+				switch (charArray[column])
 				{
-					// TODO: bundle these classes into an enum or sth
-					switch (charArray[column])
-					{
-						case OBSTACLE_TILE ->
-							gameBoard[column][row] = new Tile(new Position(column, row), new BlockedTileAppearance());
-						case RESOURCE_TILE ->
-							gameBoard[column][row] = new Tile(new Position(column, row), new ResourceTileAppearance());
-						case ARENA_TILE -> gameBoard[column][row] = new Tile(new Position(column, row),
-							new ArenaStartTileAppearance());
-						case MISSION_FINISH_TILE -> gameBoard[column][row] = new Tile(new Position(column, row),
-							new MissionFinishTileAppearance());
-						case PLAYER_START_TILE -> gameBoard[column][row] = new Tile(new Position(column, row),
-							Player.getInstance());
-						// any char besides the preceding reserved ones are accepted as placeholders.
-						default -> gameBoard[column][row] =
-							new Tile(new Position(column, row), new AccessibleTileAppearance());
-					}
+					case OBSTACLE_TILE ->
+						gameBoard[column][row] = new Tile(new Position(column, row), new BlockedTileAppearance());
+					case RESOURCE_TILE ->
+						gameBoard[column][row] = new Tile(new Position(column, row), new ResourceTileAppearance());
+					case ARENA_TILE -> gameBoard[column][row] = new Tile(new Position(column, row),
+						new ArenaStartTileAppearance());
+					case MISSION_FINISH_TILE -> gameBoard[column][row] = new Tile(new Position(column, row),
+						new MissionFinishTileAppearance());
+					case PLAYER_START_TILE -> gameBoard[column][row] = new Tile(new Position(column, row),
+						Player.getInstance());
+					// any char besides the preceding reserved ones are accepted as placeholders.
+					default -> gameBoard[column][row] =
+						new Tile(new Position(column, row), new AccessibleTileAppearance());
 				}
 			}
 		}
-		catch (final IOException e)
+		return gameBoard;
+	}
+
+
+	/**
+	 * Determines and returns the number of Lines that are in a given File.
+	 *
+	 * @param filePath The path to the file
+	 * @return number of lines in the specified file
+	 *
+	 * @precondition filePath must point to a valid file (not null)
+	 * @postcondition number of lines in the file are returned
+	 */
+	public static int getNumberOfLinesInFile (String filePath)
+	{
+		int numberOfLines = 0;
+
+		try
+		{
+			while (createBufferedReaderFromFile(filePath).readLine() != null)
+			{
+				numberOfLines++;
+			}
+		}
+		catch (IOException e)
 		{
 			throw new RuntimeException(e);
 		}
-		return gameBoard;
+		return numberOfLines;
+	}
+
+	/**
+	 * Creates a {@link BufferedReader} from the specified file
+	 *
+	 * @param filePath The Path to the file that should be converted to a {@link BufferedReader}
+	 * @return {@link BufferedReader} of the specified File
+	 *
+	 * @precondition The filePath has to point to a valid file (not null); The file must contain only characters in
+	 * the UTF-8
+	 * characterset
+	 * @postcondition A BufferedReader of the specifiedFile
+	 */
+	public static BufferedReader createBufferedReaderFromFile (String filePath)
+	{
+		try
+		{
+			return new BufferedReader(new InputStreamReader(new FileInputStream(filePath), StandardCharsets.UTF_8));
+		}
+		catch (FileNotFoundException e)
+		{
+			throw new RuntimeException(e);
+		}
 	}
 }
