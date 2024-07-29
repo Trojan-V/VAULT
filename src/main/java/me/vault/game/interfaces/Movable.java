bd198c6d22fb8293d4d1f7e49eb52fd41b19455d
Vincent <125468801+Trojan-V@users.noreply.github.com>
2024-07-29 15:31:09 +0200

    - Movable interface documentation

--- src/main/java/me/vault/game/interfaces/Movable.java
+++ src/main/java/me/vault/game/interfaces/Movable.java
@@ -1,6 +1,21 @@
 package me.vault.game.interfaces;
 
 
+import me.vault.game.model.arena.Figure;
+import me.vault.game.model.player.Player;
+
+
+/**
+ * An interface for objects that are movable.
+ * <br>
+ * For instance, {@link Figure}s and {@link Player}s are movable, as they need to be moved in the arena (and the
+ * {@link Player} also needs to move on the mission map).
+ *
+ * @author Vincent Wolf
+ * @version 1.0.0
+ * @see Placeable
+ * @since 29.07.2024
+ */
 public interface Movable extends Placeable
 {
 }
