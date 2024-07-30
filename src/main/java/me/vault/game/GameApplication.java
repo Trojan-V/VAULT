82ccd4b32fcb88d07a2c603ec25f9350f1ccab73
T.H <167987663+SurpriseDecentralizedBackup@users.noreply.github.com>
2024-07-30 20:10:53 +0200

    game application java doc

--- src/main/java/me/vault/game/GameApplication.java
+++ src/main/java/me/vault/game/GameApplication.java
@@ -2,7 +2,11 @@ package me.vault.game;
 
 
 import javafx.application.Application;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
+import me.vault.game.utility.constant.GameConstants;
+import me.vault.game.utility.jvm.JvmArgument;
 import me.vault.game.utility.jvm.JvmArgumentParser;
 import me.vault.game.utility.logging.Logger;
 import me.vault.game.view.ExitGameDialogDelegate;
@@ -36,6 +40,9 @@ public class GameApplication extends Application
 	private static final String FXML_FILENAME = "main.fxml";
 
 
+	/**
+	 * The main {@link Stage} of the application on which the {@link Scene}s are displayed.
+	 */
 	private static Stage mainStage = null;
 
 
@@ -44,6 +51,9 @@ public class GameApplication extends Application
 	 * method, as the runtime environment will automatically invoke it anyway in a JavaFX project. Declaring it explicitly doesn't hurt though.
 	 *
 	 * @param args Contains the JVM arguments which have been passed into the program at the start.
+	 *
+	 * @precondition The args must be valid ({@link JvmArgument}).
+	 * @postcondition The application is launched with the modifications given by the args.
 	 */
 	public static void main (final String[] args)
 	{
@@ -52,6 +62,14 @@ public class GameApplication extends Application
 	}
 
 
+	/**
+	 * Returns the {@link GameApplication#mainStage} of the application.
+	 *
+	 * @return mainStage of {@link GameApplication}
+	 *
+	 * @precondition the {@link GameApplication#start(Stage)} has been called.
+	 * @postcondition the current {@link GameApplication#mainStage} is returned.
+	 */
 	public static Stage getStage ()
 	{
 		return mainStage;
@@ -62,19 +80,24 @@ public class GameApplication extends Application
 	 * Loads the {@code .fxml} files which are located in {@code ./src/main/java/resources/me/vault/game} and starts the GUI powered by JavaFX.
 	 *
 	 * @param stage The {@link Stage} which will be displayed in the GUI window which pops up.
+	 *
+	 * @precondition stage != null.
+	 * @postcondition The parameter stage is set as the {@link GameApplication#mainStage}. The stage is set
+	 * non-resizable and the title is set to {@link GameConstants#WINDOW_TITLE}; The main menu is shown; An event
+	 * Handler is added for the {@link Stage#setOnCloseRequest(EventHandler)} that shows the exit dialog upon
+	 * receiving the close request.
 	 */
 	@Override
 	public void start (final Stage stage)
 	{
 		mainStage = stage;
-		stage.setResizable(false);
-		stage.setTitle(WINDOW_TITLE);
+		mainStage.setResizable(false);
+		mainStage.setTitle(WINDOW_TITLE);
 		MainMenuDelegate.show();
 
-		stage.setOnCloseRequest(evt -> {
+		mainStage.setOnCloseRequest(evt -> {
 			evt.consume();
 			ExitGameDialogDelegate.show();
 		});
 	}
-
 }
