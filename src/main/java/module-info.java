60971f08db988a459bbd6d0b49c0d21605dc8df5
Vincent <125468801+Trojan-V@users.noreply.github.com>
2024-07-31 17:44:46 +0200

    - added all packages to module-info.java
    - configured maven javadoc plugin for convenient javadoc generation,
    including custom tags
    - no tex document generation yet

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -11,25 +11,51 @@ module me.vault.game {
 	requires com.google.gson;
 
 	exports me.vault.game;
+	exports me.vault.game.control;
+	exports me.vault.game.model;
+	exports me.vault.game.view;
+	exports me.vault.game.utility;
+
+	exports me.vault.game.model.arena;
 	exports me.vault.game.model.artifact;
-	exports me.vault.game.utility.interfaces;
+	exports me.vault.game.model.city;
 	exports me.vault.game.model.currency;
+	exports me.vault.game.model.energy;
+	exports me.vault.game.model.gameboard;
+	exports me.vault.game.model.network;
+	exports me.vault.game.model.troop;
+
+	exports me.vault.game.model.artifact.impl;
+	exports me.vault.game.model.city.impl;
+	exports me.vault.game.model.energy.impl;
+	exports me.vault.game.model.gameboard.tile;
+	exports me.vault.game.model.gameboard.tile.impl;
+	exports me.vault.game.model.troop.impl;
+
+	exports me.vault.game.utility.comparators;
+	exports me.vault.game.utility.concurrency;
 	exports me.vault.game.utility.datatypes;
 	exports me.vault.game.utility.exception;
+	exports me.vault.game.utility.fx;
+	exports me.vault.game.utility.interfaces;
 	exports me.vault.game.utility.jvm;
 	exports me.vault.game.utility.logging;
 	exports me.vault.game.utility.loading;
-	exports me.vault.game.model.artifact.impl;
-	exports me.vault.game.view;
-	exports me.vault.game.control;
-	exports me.vault.game.model.city;
-	exports me.vault.game.view.city.building;
+	exports me.vault.game.utility.math;
+
+	exports me.vault.game.utility.interfaces.constant;
+
+	exports me.vault.game.view.arena;
 	exports me.vault.game.view.city;
-	exports me.vault.game.model;
-	exports me.vault.game.utility.fx;
-	exports me.vault.game.model.troop;
-	exports me.vault.game.model.arena;
-	exports me.vault.game.model.energy;
+	exports me.vault.game.view.menu;
+	exports me.vault.game.view.mission;
+	exports me.vault.game.view.newgame;
+
+	exports me.vault.game.view.city.building;
+
+
+
+
 
 	opens me.vault.game to javafx.fxml;
 	opens me.vault.game.utility.loading to javafx.fxml, com.google.gson;
@@ -44,27 +70,15 @@ module me.vault.game {
 	opens me.vault.game.view.mission to javafx.fxml;
 	opens me.vault.game.model.arena to javafx.fxml;
 	opens me.vault.game.model.energy to javafx.fxml;
-	exports me.vault.game.view.mission;
 	opens me.vault.game.utility.fx to javafx.fxml;
-	exports me.vault.game.model.gameboard;
 	opens me.vault.game.model.gameboard to javafx.fxml;
-	exports me.vault.game.model.gameboard.tile;
 	opens me.vault.game.model.gameboard.tile to javafx.fxml;
-	exports me.vault.game.utility;
-	exports me.vault.game.utility.math;
-	exports me.vault.game.model.gameboard.tile.impl;
 	opens me.vault.game.model.gameboard.tile.impl to javafx.fxml;
-	exports me.vault.game.model.city.impl;
 	opens me.vault.game.model.city.impl to javafx.fxml;
 	opens me.vault.game.model to javafx.fxml;
-	exports me.vault.game.utility.interfaces.constant;
 	opens me.vault.game.utility to com.google.gson, javafx.fxml;
-	exports me.vault.game.utility.concurrency;
 	opens me.vault.game.utility.math to com.google.gson, javafx.fxml;
-	exports me.vault.game.view.arena;
 	opens me.vault.game.view.arena to javafx.fxml;
-	exports me.vault.game.view.menu;
 	opens me.vault.game.view.menu to javafx.fxml;
-	exports me.vault.game.view.newgame;
 	opens me.vault.game.view.newgame to javafx.fxml;
 }
