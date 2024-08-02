3a5cf8cbcce540c1fb5aee9e04bd3b16e8f3ff82
T.H <167987663+SurpriseDecentralizedBackup@users.noreply.github.com>
2024-08-02 18:59:30 +0200

    add javadoc to Network constants

--- src/main/java/me/vault/game/utility/interfaces/constant/NetworkConstants.java
+++ src/main/java/me/vault/game/utility/interfaces/constant/NetworkConstants.java
@@ -1,30 +1,40 @@
 package me.vault.game.utility.interfaces.constant;
 
-//TODO: javadoc
 /**
+ * This interface provides constants for the use in the networkimplementation of this application.
  *
+ *  @author Vincent Wolf, Lasse-Leander Hillen, Timothy Hoegen-Jupp, Alexander Goethel
+ *  @version 1.0.0
+ *  @since 02.08.2024
  */
 public interface NetworkConstants
 {
+	/**
+	 * The smallest ephemeral port (see https://en.wikipedia.org/wiki/Ephemeral_port).
+	 */
 	int MINIMUM_PORT_NUMBER = 49152;
 
 
-	int MAXIMUM_PORT_NUMBER = 65536;
+	/**
+	 * The larges ephemeral port (see https://en.wikipedia.org/wiki/Ephemeral_port).
+	 */
+	int MAXIMUM_PORT_NUMBER = 65535;
 
-// Messages
-	String ERROR_SERVER_SOCKET = "Error with ServerSocket";
 
+	/**
+	 * Error message when the connection could not be established.
+	 */
 	String ERROR_ACCEPTING = "error accepting";
 
-	String CLIENT_CONNECTED = "client connected";
 
+	/**
+	 * Message when a connection was accepted by the Peer.
+	 */
 	String ACCEPTED = "Accepted";
 
-	String ERROR_ACCEPT = "Error accept!";
 
-	String WAIT_FOR_CONNECTIONREQUEST = "Wait for Connection request";
-
-	String ERROR_SERVER_SOCKET_CONSTRUCTOR = "Error ServerSocket-Constructor!";
-
+	/**
+	 * Error message that is displayed to the user.
+	 */
 	String INVALID_CONNETCTION_ATTEMPT = "Invalid connetction attempt.";
 }
