33c57e0b12d4e6b74a76f480c81c1f80060bff1b
T.H <167987663+SurpriseDecentralizedBackup@users.noreply.github.com>
2024-08-02 19:02:57 +0200

    add javadoc to ThreadUtil

--- src/main/java/me/vault/game/utility/concurrency/ThreadUtil.java
+++ src/main/java/me/vault/game/utility/concurrency/ThreadUtil.java
@@ -1,9 +1,23 @@
 package me.vault.game.utility.concurrency;
 
 
+/**
+ * Provides methods for the use in the implementation of concurrency.
+ *
+ * @author Vincent Wolf, Lasse-Leander Hillen, Timothy Hoegen-Jupp, Alexander Goethel
+ * @version 1.0.0
+ * @since 02.08.2024
+ */
 public class ThreadUtil
 {
-		//TODO: javadoc
+	/**
+	 * Lets the calling thread sleep for the specified amount of time.
+	 *
+	 * @param sleepTime time that the calling thread should sleep.
+	 *
+	 * @precondition none.
+	 * @postcondition The calling thread sleeps (is unresponsive) for the specified amount of time.
+	 */
 	public static void sleepThread(long sleepTime)
 	{
 		try
